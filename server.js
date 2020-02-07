14fc6a4f5e39abeaba0cf2f6191f7f3f2edc0fbe
Ethan Irvine <edog.irvine@gmail.com>
2020-02-06 20:16:47 -0700

    created a api for tables/waitlist

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ var PORT = process.env.PORT || 3000;
         console.log("App listening on PORT " + PORT);
     });
 // }
+const reservation = [];
+const waitlist = [];
 
-
-// runApp();
+app.get("/api/tables", function(req, res) {
+    return res.json(reservation);
+  });
+  app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+  });
