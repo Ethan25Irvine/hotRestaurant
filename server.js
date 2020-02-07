bb691f6c5cecc2e10341f73e3b3cb7afd16b1243
Ethan Irvine <edog.irvine@gmail.com>
2020-02-06 19:58:42 -0700

    now we have a server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require("express");
-
 var app = express();
+
 var PORT = process.env.PORT || 3000;
+// function runApp (){
+    // Sets up the Express app to handle data parsing
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
+// }
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+
+// runApp();
